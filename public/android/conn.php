fca1342b7ecaefefa9491174de091c198a609b29
andinarru <andinarru9@gmail.com>
2021-08-17 20:06:15 +0800

    test connect

--- public/android/conn.php
+++ public/android/conn.php
@@ -1,9 +1,9 @@
 <?php
- $con = mysqli_connect("localhost","u349600776_emasjid","Hostinger");
- mysqli_select_db($con,"u349600776_emasjid");
+$con = mysqli_connect("localhost", "u349600776_emasjid", "Hostinger");
+mysqli_select_db($con, "u349600776_emasjid");
 
-//   if($con == false){
-//      echo "not connected";
-//  }else{
-//      echo "connected";
-//  }
+if ($con == false) {
+    echo "not connected";
+} else {
+    echo "connected";
+}
