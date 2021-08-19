1edb513271a5e88dc91a642d3ff94ea083aeca10
andinarru <andinarru9@gmail.com>
2021-08-19 19:25:44 +0800

    perbaikan untuk api android

--- public/android/api.php
+++ public/android/api.php
@@ -6,34 +6,36 @@ header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 // mysqli_set_charset('utf8');
 
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    if(json_decode(file_get_contents('php://input'), true)){
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (json_decode(file_get_contents('php://input'), true)) {
         $_POST = json_decode(file_get_contents('php://input'), true);
     };
-    $date=date("Ymd-h_i_s");
-    $case=$_POST['case'];
-    switch($case){
+    $date = date("Ymd-h_i_s");
+    $case = $_POST['case'];
+    switch ($case) {
 
-#----------------------------------------------------------------------------------------------------------------------------------------
-case "masjid":
-    $type_query = "show";
+            #----------------------------------------------------------------------------------------------------------------------------------------
+        case "masjid":
+            $type_query = "show";
 
-    $query = "SELECT * FROM masjid";
-    
-    include './res.php';
-die();
-break;
+            $query =  "SELECT * FROM masjid 
+                JOIN kategori ON masjid.id_kategory=kategori.id_kategory 
+                JOIN bank ON masjid.id_bank=bank.id_bank 
+                JOIN infaq ON masjid.id_infaq=infaq.id_infaq";
 
-#----------------------------------------------------------------------------------------------------------------------------------------
-case $case:
-    $type_query = "show";
-    $masjid_id = $_POST['masjid_id'];
+            include './res.php';
+            die();
+            break;
 
-    $query = "SELECT * FROM $case WHERE masjid_id='$masjid_id'";
-    
-    include './res.php';
-die();
-break;
+            #----------------------------------------------------------------------------------------------------------------------------------------
+        case $case:
+            $type_query = "show";
+            $masjid_id = $_POST['masjid_id'];
 
+            $query = "SELECT * FROM $case WHERE masjid_id='$masjid_id'";
+
+            include './res.php';
+            die();
+            break;
     }
 }
